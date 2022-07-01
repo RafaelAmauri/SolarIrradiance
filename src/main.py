be17f47aaa2ea0a4d3efd8a151b554a523acbd47
Rafael <amauri.rafael33@gmail.com>
2022-07-01 19:14:38 -0300

    [refact]: Bump default number of threads used from 4 to 6

--- src/main.py
+++ src/main.py
@@ -6,6 +6,6 @@ pred.tseries_start = 2015
 pred.tseries_end = 2020
 pred.num_training_years = 2
 pred.charts_export_location = "./predictions"
-pred.num_threads = 4
+pred.num_threads = 6
 
 pred.plot_predictions(["A555", "A510", "A513", "A521", "A552", "A556", "A557", "A729"], "./stations")
